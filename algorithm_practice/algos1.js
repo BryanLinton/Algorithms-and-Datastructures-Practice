d7f452733c10884e01dd4781629dce81cfca1cef
Bryan <btlinton99@gmail.com>
2020-01-28 15:27:12 -0800

    more algos

--- algorithm_practice/algos1.js
+++ algorithm_practice/algos1.js
@@ -1,11 +1,46 @@
-//Setting and Swapping
-myNumber = 42
-myName = "Bryan"
-function swap(){
-    myNumber = "Bryan";
-    myName = 42;
-    return myNumber, myName;
-}
-console.log(swap()); 
+// //Setting and Swapping
+// myNumber = 42
+// myName = "Bryan"
+// function swap(){
+//     myNumber = "Bryan";
+//     myName = 42;
+//     return myNumber, myName;
+// }
+// console.log(swap()); 
 
-//
+// //Print -52 to 1066
+// function countup(){
+//     for(var i = -52; i <= 1066; i++){
+//         console.log(i);
+//     }
+// }
+// console.log(countup());
+
+// //Don't Worry, Be Happy
+// function beCheerful(){
+//     for(var i = 0; i < 98; i++){
+//         console.log("good morning!");
+//     }
+// }
+// console.log(beCheerful());
+
+//Multiples of Three – but Not All
+// function multi3(){
+//     for(var i = -300; i <= 0; i += 3){
+//         if(i == -3 || i == -6){
+//             continue;
+//         }
+//         else(console.log(i));
+//     }
+// }
+// console.log(multi3());
+
+//Printing Integers with While
+function whilecounting(){
+    var i = 1999;
+    while (i < 5280){
+        i++;
+        console.log(i);
+    }
+}
+console.log(whilecounting());
