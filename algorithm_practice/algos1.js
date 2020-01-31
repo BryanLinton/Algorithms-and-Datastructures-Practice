e82429cbd29568277c26d4cc60d4aaca5b6d830c
Bryan <btlinton99@gmail.com>
2020-01-31 15:42:50 -0800

    more algos

--- algorithm_practice/algos1.js
+++ algorithm_practice/algos1.js
@@ -35,12 +35,73 @@
 // }
 // console.log(multi3());
 
-//Printing Integers with While
-function whilecounting(){
-    var i = 1999;
-    while (i < 5280){
-        i++;
-        console.log(i);
+// //Printing Integers with While
+// function whilecounting(){
+//     var i = 1999;
+//     while (i < 5280){
+//         i++;
+//         console.log(i);
+//     }
+// }
+// console.log(whilecounting());
+
+// //You Say it's your Birthday
+// function birthday(m,d){
+//     if (m === 7 && d === 2){
+//         console.log("How did you know!?")
+//     }
+//     else{
+//         console.log("Just another Day....")
+//     }
+// }
+// console.log(birthday(9,18));
+
+// //Leap Year
+// function leap(y){
+//     if (y % 4 !== 0 || y % 100 === 0){
+//         console.log("Not a leap year");
+//     }
+//     else{
+//         console.log("Issa leap year");
+//     }
+// }
+// console.log(leap(1100));
+
+// //Print and Count
+// function printAndCount(){
+//     var count = 0
+//     for(var i = 512; i <= 4096; i++){
+//         if (i % 5 === 0){
+//            count += 1;
+//            console.log(i); 
+//         }
+//     }
+//     console.log(count);
+// }
+// console.log(printAndCount());
+
+// //Multiples of Six
+// function multiofsix(){
+//     var i = 0
+//     while(i <= 60000){
+//         i++;
+//         if(i % 6 === 0){
+//             console.log(i);
+//         }
+//     }
+// }
+// console.log(multiofsix());
+
+//Counting, the Dojo Way
+function dojoCount(){
+    for(var i = 0; i <= 100; i++){
+        if(i % 10 === 0){
+            console.log(" Dojo");
+        }
+        if(i % 5 === 0){
+            console.log("Coding");
+        }
+        else(console.log(i));
     }
 }
-console.log(whilecounting());
+console.log(dojoCount());
